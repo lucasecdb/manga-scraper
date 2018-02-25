4babf7b8c9380d4d318073f3edc20d5be6f309e5
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-02-25 15:50:54 -0300

    generalizing image pipeline

--- manga/pipelines.py
+++ manga/pipelines.py
@@ -6,15 +6,18 @@ class MangaImagesPipeline(ImagesPipeline):
 
     def get_media_requests(self, item, info):
         image_urls = item.get('image_urls', [])
+        image_names = item.get(
+            'image_names', [i + 1 for i in range(len(image_urls))])
 
         for i in range(len(image_urls)):
             url = image_urls[i]
+            name = image_names[i]
 
             yield scrapy.Request(url, meta={
                 'folder': info.spider.name,
                 'chapter': item['chapter'],
-                'index': i + 1
+                'name': name
             })
 
     def file_path(self, request, response=None, info=None):
-        return '%s/%s/%02d.jpg' % (request.meta['folder'], request.meta['chapter'], request.meta['index'])
+        return '%s/%s/%02d.jpg' % (request.meta['folder'], request.meta['chapter'], request.meta['name'])
